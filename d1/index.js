a687134f9068bcea2b7029bd8b9055c92ee394dd
giooooo <gbperalta1@up.edu.ph>
2020-10-25 17:53:58 +0800

    add solution for day 1

---
+++ d1/index.js
@@ -0,0 +1,35 @@
+const fs = require('fs').promises;
+
+async function start() {
+    try {
+        const data = await fs.readFile('./input.txt', 'utf-8');
+    
+        const input = data.toString();
+        const array = input.split('\n');
+        const frequency = {};
+    
+        let acc = 0, index = 0;
+    
+        while(1) {
+    
+            const element = Number(array[index]);
+    
+            acc += element;
+    
+            frequency[acc] = frequency[acc] ? frequency[acc] + 1 : 1;    
+    
+            if (frequency[acc] === 2) {
+                console.log(acc);   
+                break;
+            }
+    
+            index = (index + 1) % array.length;
+        }
+    
+    }
+    catch (err) {
+        throw err;
+    }
+}
+
+start();
